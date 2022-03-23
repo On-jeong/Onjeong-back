f4d539ad40e8cf758422a5d38a185bdb251ac257
haeun <gkdms1120s@gmail.com>
2022-03-23 19:13:40 +0900

    first commit

---
+++ src/test/java/com/example/onjeong/OnjeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onjeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnjeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
