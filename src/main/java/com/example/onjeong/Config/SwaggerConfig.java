1b3a8a435ca27f1c99b0a12fc25b023ca38fb29f
haeun <gkdms1120s@gmail.com>
2022-04-02 19:01:58 +0900

    swagger error resolve

--- src/main/java/com/example/onjeong/Config/SwaggerConfig.java
+++ src/main/java/com/example/onjeong/Config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.example.onjeong.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,7 +14,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    }
 
     @Bean
     public Docket restAPI() {
