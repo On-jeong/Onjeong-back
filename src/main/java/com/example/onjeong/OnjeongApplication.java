ede404d318600516305402ad30d6dc3d66b2ee1b
haeun <gkdms1120s@gmail.com>
2022-06-22 14:33:12 +0900

    feature : mail, quesiton 기능 추가

--- src/main/java/com/example/onjeong/OnjeongApplication.java
+++ src/main/java/com/example/onjeong/OnjeongApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+//@ComponentScan(basePackages = "com.example.onjeong")
 @EnableSwagger2
 @ComponentScan(basePackages = "com.example.onjeong.controller")
 @SpringBootApplication
