ef0695c338c23583943a71449425bf6d55bd0a4b
haeun <gkdms1120s@gmail.com>
2022-08-02 23:41:05 +0900

    batch 기능 추가 및 포인트 지급로직 수정

--- src/main/java/com/example/onjeong/OnjeongApplication.java
+++ src/main/java/com/example/onjeong/OnjeongApplication.java
@@ -1,10 +1,14 @@
 package com.example.onjeong;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
+@EnableScheduling     // 스케쥴러 기능 활성화
+@EnableBatchProcessing  // 배치 기능 활성화
 @SpringBootApplication
 public class OnjeongApplication {
 
