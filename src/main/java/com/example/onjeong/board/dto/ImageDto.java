725972fb5343cb85c61e3d549fa5ff159efeaf39
EC2 Default User <ec2-user@ip-172-31-35-100.ap-northeast-2.compute.internal>
2022-08-30 05:04:55 +0000

    src/main/java/com/example/onjeong/board/dto/ImageDto.java

--- src/main/java/com/example/onjeong/board/dto/ImageDto.java
+++ src/main/java/com/example/onjeong/board/dto/ImageDto.java
@@ -1,7 +1,10 @@
 package com.example.onjeong.board.dto;
 
 import lombok.AllArgsConstructor;
+<<<<<<< Updated upstream:src/main/java/com/example/onjeong/board/dto/ImageDto.java
 import lombok.Builder;
+=======
+>>>>>>> Stashed changes:src/main/java/com/example/onjeong/profile/dto/ProfileImageFileDto.java
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
