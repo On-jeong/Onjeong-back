c24f2f70cc3681e939d716ce5b92dadf6d284775
Junhui0u0 <qaz74792@gmail.com>
2022-09-03 15:55:08 +0900

    바뀐 s3에 대한 수정 코드

--- src/main/java/com/example/onjeong/board/service/BoardService.java
+++ src/main/java/com/example/onjeong/board/service/BoardService.java
@@ -29,7 +29,7 @@ public class BoardService {
     private final UserRepository userRepository;
     private final S3Uploader s3Uploader;
 
-    @Value("https://onjeong-bucket.s3.ap-northeast-2.amazonaws.com/")
+    @Value("https://onjeong.s3.ap-northeast-2.amazonaws.com/")
     private String AWS_S3_BUCKET_URL;
 
     //오늘의 기록 모두 가져오기
