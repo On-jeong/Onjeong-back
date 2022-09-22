81aa9679c6f380d70d064e513ed5ea319816a53a
Junhui0u0 <qaz74792@gmail.com>
2022-09-22 18:08:06 +0900

    ResultResponse 부분

--- src/main/java/com/example/onjeong/result/ResultResponse.java
+++ src/main/java/com/example/onjeong/result/ResultResponse.java
@@ -1,2 +1,23 @@
-package com.example.onjeong.result;public class ResultResponse {
+package com.example.onjeong.result;
+
+import lombok.Getter;
+
+@Getter
+public class ResultResponse {
+
+    private int status;
+    private String code;
+    private String message;
+    private Object data;
+
+    public static ResultResponse of(ResultCode resultCode, Object data) {
+        return new ResultResponse(resultCode, data);
+    }
+
+    public ResultResponse(ResultCode resultCode, Object data) {
+        this.status = resultCode.getStatus();
+        this.code = resultCode.getCode();
+        this.message = resultCode.getMessage();
+        this.data = data;
+    }
 }
