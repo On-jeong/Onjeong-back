879b2889314fcb47a0075a24f8fbf546866e2a95
haeun <gkdms1120s@gmail.com>
2023-03-21 16:54:21 +0900

    feat: 오래된 알림 삭제 배치 등록

---
+++ module-batch/src/main/java/com/example/onjeong/job/NotificationDeleteJob.java
@@ -0,0 +1,66 @@
+package com.example.onjeong.job;
+
+import com.example.onjeong.anniversary.domain.Anniversary;
+import com.example.onjeong.config.BatchConfig;
+import com.example.onjeong.notification.domain.Notifications;
+import com.example.onjeong.processor.AnniversaryNotificationProcessor;
+import com.example.onjeong.writer.AnniversaryNotificationItemWriter;
+import com.example.onjeong.writer.NotificationsDeleteItemWriter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+
+@Slf4j // log 사용을 위한 lombok 어노테이션
+@RequiredArgsConstructor // 생성자 DI를 위한 lombok 어노테이션
+@Configuration
+public class NotificationDeleteJob {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+    private final BatchConfig batchConfig;
+
+    private final NotificationsDeleteItemWriter notificationsDeleteItemWriter;
+
+    @Bean
+    public Job notificationsDeleteJob() {
+        return jobBuilderFactory.get("notificationsDeleteJob")
+                .start(notificationsDeleteStep())
+                .build();
+    }
+
+    @Bean
+    public Step notificationsDeleteStep() {
+        return stepBuilderFactory.get("notificationsDeleteStep")
+                .<Notifications, Notifications>chunk(batchConfig.getChunk())
+                .reader(notificationsReader())
+                .writer(notificationsDeleteItemWriter)
+                .build();
+    }
+
+    @Bean
+    public JpaPagingItemReader<Notifications> notificationsReader() {
+        HashMap<String, Object> paramValues = new HashMap<>();
+        paramValues.put("date", LocalDateTime.now().minusDays(4));
+
+        return new JpaPagingItemReaderBuilder<Notifications>()
+                .name("notificationsReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(batchConfig.getChunk())
+                .queryString("select n from Notifications n WHERE n.notificationTime <= :date")
+                .parameterValues(paramValues)
+                .build();
+    }
+}
--- module-batch/src/main/java/com/example/onjeong/scheduler/JobScheduler.java
+++ module-batch/src/main/java/com/example/onjeong/scheduler/JobScheduler.java
@@ -1,5 +1,6 @@
 package com.example.onjeong.scheduler;
 import com.example.onjeong.job.AnniversaryNotificationJob;
+import com.example.onjeong.job.NotificationDeleteJob;
 import com.example.onjeong.job.WeeklyQuestionBuildJob;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.JobParameter;
@@ -18,6 +19,7 @@ public class JobScheduler {
     private final JobLauncher jobLauncher;
     private final WeeklyQuestionBuildJob weeklyQuestionBuildJob;
     private final AnniversaryNotificationJob anniversaryNotificationJob;
+    private final NotificationDeleteJob notificationDeleteJob;
 
     // 매주 월요일 0시에 실행
     @Scheduled(cron="0 0 0 ? * MON")
@@ -38,6 +40,18 @@ public class JobScheduler {
             Map<String, JobParameter> confMap = new HashMap<>();
             confMap.put("requestDate", new JobParameter(System.currentTimeMillis()));
             jobLauncher.run(anniversaryNotificationJob.notificationBuildJob(), new JobParameters(confMap));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // 매일 새벽 4시에 실행
+    @Scheduled(cron="0 0 4 1/1 * ?")
+    public void runNotificationDeleteJob() {
+        try {
+            Map<String, JobParameter> confMap = new HashMap<>();
+            confMap.put("requestDate", new JobParameter(System.currentTimeMillis()));
+            jobLauncher.run(notificationDeleteJob.notificationsDeleteJob(), new JobParameters(confMap));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
